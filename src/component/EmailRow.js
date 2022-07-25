6d73cf0da03557ac9135081ed8aa72bbd3ee26c5
Shubham <abdshubham13@gmail.com>
2022-07-25 20:09:43 +0530

    delete implemented

--- src/component/EmailRow.js
+++ src/component/EmailRow.js
@@ -5,6 +5,11 @@ import { IconButton } from "@mui/material";
 import StarOutlineIcon from "@mui/icons-material/StarOutline";
 import { useNavigate } from "react-router-dom";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
+import { formatEmail } from "../Util/UserNameFormat";
+import { useDispatch } from "react-redux";
+import { fetchInbox, fetchSent } from "../features/MailAction";
 const EmailRow = ({
   type,
   title,
@@ -15,16 +20,40 @@ const EmailRow = ({
   key,
   read,
 }) => {
-  console.log(read);
+  const mail = formatEmail(localStorage.getItem("userMail"));
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const openMailHandler = () => {
     navigate(`/mail/${type}/${id}`);
   };
 
+  const deleteMailHandler = () => {
+    console.log(id);
+
+    axios
+      .delete(
+        `https://mail-box-client-948c9-default-rtdb.firebaseio.com/${mail}/${type}/${id}.json`
+      )
+      .then((res) => {
+        console.log(res);
+        if (type === "inbox") {
+          dispatch(fetchInbox());
+        }
+        if (type === "sent") {
+          dispatch(fetchSent());
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div onClick={openMailHandler} className={`emailRow ${read ? "read" : ""}`}>
+    <div className={`emailRow ${read ? "read" : ""}`}>
       <div className={`mailRow__options`}>
-        {!read && <FiberManualRecordIcon className="read_dot" />}
+        {!read && type === "inbox" && (
+          <FiberManualRecordIcon className="read_dot" />
+        )}
         <IconButton>
           <CheckBoxOutlineBlankIcon />
         </IconButton>
@@ -32,7 +61,7 @@ const EmailRow = ({
           <StarOutlineIcon />
         </IconButton>
       </div>
-      <div className="emailRow__mailContent">
+      <div onClick={openMailHandler} className="emailRow__mailContent">
         <h4 className="emailRow__title">{title}</h4>
         <div className="emailRow__message">
           <h5>
@@ -42,6 +71,7 @@ const EmailRow = ({
         </div>
         <div className="email__description">{time}</div>
       </div>
+      <DeleteIcon onClick={deleteMailHandler} />
     </div>
   );
 };
